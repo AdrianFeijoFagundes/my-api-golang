fb8c8b976aad510287f437cf627fd2f520512b85
adrian fagundes <adrianfeijofagundes@gmail.com>
2024-10-30 16:01:46 -0300

    CREADTE: routes names

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -16,13 +17,39 @@ func main() {
 	e.Use(middleware.Recover())
 
 	// Routes
-	e.GET("/students", getStudents)
+	e.GET("/students", getAllStudents)
+	e.POST("/students", createStudent)
+	e.GET("/students/:id", getStudent)
+	e.PUT("/students/:id", updateStudent)
+	e.DELETE("/students/:id", deleteStudent)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
 // Handler
-func getStudents(c echo.Context) error {
+func getAllStudents(c echo.Context) error {
 	return c.String(http.StatusOK, "List of all students\n")
+}
+func createStudent(c echo.Context) error {
+	return c.String(http.StatusOK, "Create students\n")
+}
+func getStudent(c echo.Context) error {
+	id := c.Param("id") // Pega o parametro que foi passado
+	message := "GET" + getId(id)
+	return c.String(http.StatusOK, message)
+}
+func updateStudent(c echo.Context) error {
+	id := c.Param("id")
+	message := "UPDATE" + getId(id)
+	return c.String(http.StatusOK, message)
+}
+func deleteStudent(c echo.Context) error {
+	id := c.Param("id")
+	message := "DELETE" + getId(id)
+	return c.String(http.StatusOK, message)
+}
+
+func getId(id string) string {
+	return fmt.Sprintf("%s student \n", id)
 }
