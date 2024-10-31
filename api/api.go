4540df62fe013388dad1580f9d9dc8ef6fe7615a
adrian fagundes <adrianfeijofagundes@gmail.com>
2024-10-31 10:54:36 -0300

    CREATE: DB instance

--- api/api.go
+++ api/api.go
@@ -7,12 +7,11 @@ import (
 	"github.com/adrian-feijo-fagundes/my-api-golang/db"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-	"gorm.io/gorm"
 )
 
 type API struct {
 	Echo *echo.Echo
-	DB   *gorm.DB
+	DB   *db.StudentHandler
 }
 
 func NewServer() *API {
@@ -20,17 +19,18 @@ func NewServer() *API {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	db := db.Init()
+	database := db.Init()
+	studentDB := db.NewStundentHandler(database)
 
-	return &API{Echo: e, DB: db}
+	return &API{Echo: e, DB: studentDB}
 }
 
 func (api *API) ConfigureRoutes() {
-	api.Echo.GET("/students", getStudents)
-	api.Echo.POST("/students", createStudent)
-	api.Echo.GET("/students/:id", getStudent)
-	api.Echo.PUT("/students/:id", updateStudent)
-	api.Echo.DELETE("/students/:id", deleteStudent)
+	api.Echo.GET("/students", api.getStudents)
+	api.Echo.POST("/students", api.createStudent)
+	api.Echo.GET("/students/:id", api.getStudent)
+	api.Echo.PUT("/students/:id", api.updateStudent)
+	api.Echo.DELETE("/students/:id", api.deleteStudent)
 }
 
 func (api *API) Start() error {
@@ -38,35 +38,35 @@ func (api *API) Start() error {
 }
 
 // Handler
-func getStudents(c echo.Context) error {
-	students, err := db.GetStudents()
+func (api *API) getStudents(c echo.Context) error {
+	students, err := api.DB.GetStudents()
 	if err != nil {
 		return c.String(http.StatusNotFound, "Failed to get students")
 	}
 	return c.JSON(http.StatusOK, students)
 }
-func createStudent(c echo.Context) error {
+func (api *API) createStudent(c echo.Context) error {
 	student := db.Student{}
 	if err := c.Bind(&student); err != nil {
 		return err
 	}
-	if err := db.AddStudent(student); err != nil { // FORMA "simplificada de tratar um erro"
+	if err := api.DB.AddStudent(student); err != nil { // FORMA "simplificada de tratar um erro"
 		return c.String(http.StatusInternalServerError, "Error to create students\n")
 	}
 
 	return c.String(http.StatusOK, "Create students\n")
 }
-func getStudent(c echo.Context) error {
+func (api *API) getStudent(c echo.Context) error {
 	id := c.Param("id") // Pega o parametro que foi passado
 	message := "GET " + getId(id)
 	return c.String(http.StatusOK, message)
 }
-func updateStudent(c echo.Context) error {
+func (api *API) updateStudent(c echo.Context) error {
 	id := c.Param("id")
 	message := "UPDATE " + getId(id)
 	return c.String(http.StatusOK, message)
 }
-func deleteStudent(c echo.Context) error {
+func (api *API) deleteStudent(c echo.Context) error {
 	id := c.Param("id")
 	message := "DELETE " + getId(id)
 	return c.String(http.StatusOK, message)
