b682662c6c3e659266b2212d877834dedf42d224
adrian fagundes <adrianfeijofagundes@gmail.com>
2024-10-31 08:50:54 -0300

    Add error handling

--- main.go
+++ main.go
@@ -37,7 +37,9 @@ func createStudent(c echo.Context) error {
 	if err := c.Bind(student); err != nil {
 		return err
 	}
-	db.AddStudent(student)
+	if err := db.AddStudent(student); err != nil {
+		return c.String(http.StatusInternalServerError, "Error to create students\n")
+	}
 
 	return c.String(http.StatusOK, "Create students\n")
 }
