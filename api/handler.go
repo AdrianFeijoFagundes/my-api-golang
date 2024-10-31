2847f115e6bb8ad29a0e2c50fa95c1f10c55ab71
adrian fagundes <adrianfeijofagundes@gmail.com>
2024-10-31 12:31:02 -0300

    Create handler to update student by id

--- api/handler.go
+++ api/handler.go
@@ -26,7 +26,7 @@ func (api *API) createStudent(c echo.Context) error {
 		return c.String(http.StatusInternalServerError, "Error to create students\n")
 	}
 
-	return c.String(http.StatusOK, "Create students\n")
+	return c.String(http.StatusOK, "Success to Create Stundent\n")
 }
 func (api *API) getStudent(c echo.Context) error {
 	id, err := strconv.Atoi(c.Param("id")) // Pega o parametro que foi passado
@@ -43,10 +43,52 @@ func (api *API) getStudent(c echo.Context) error {
 	return c.JSON(http.StatusOK, student)
 }
 func (api *API) updateStudent(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id")) // Pega o parametro que foi passado
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "Failed to get student ID")
+	}
 
-	return c.String(http.StatusOK, "")
+	receivedStudent := db.Student{}
+	if err := c.Bind(&receivedStudent); err != nil {
+		return err
+	}
+
+	updatingStudent, err := api.DB.GetStudent(id)
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return c.String(http.StatusNotFound, "Student id not found")
+	}
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "Failed to get student")
+	}
+
+	student := updateStudentInfo(receivedStudent, updatingStudent)
+
+	if err := api.DB.UpdateStudent(student); err != nil {
+		return c.String(http.StatusInternalServerError, "Failed to save student")
+	}
+	return c.String(http.StatusOK, "Success to Update student")
 }
 func (api *API) deleteStudent(c echo.Context) error {
 
 	return c.String(http.StatusOK, "")
+}
+
+func updateStudentInfo(receivedStudent, student db.Student) db.Student {
+	if receivedStudent.Name != "" {
+		student.Name = receivedStudent.Name
+	}
+	if receivedStudent.CPF != "" {
+		student.CPF = receivedStudent.CPF
+	}
+	if receivedStudent.Email != "" {
+		student.Email = receivedStudent.Email
+	}
+	if receivedStudent.Age > 0 {
+		student.Age = receivedStudent.Age
+	}
+	if receivedStudent.Active != student.Active {
+		student.Active = receivedStudent.Active
+	}
+
+	return student
 }
