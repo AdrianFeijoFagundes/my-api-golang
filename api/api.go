cf2e4f9aef24b8ff3c2a28795df56135578252df
adrian fagundes <adrianfeijofagundes@gmail.com>
2024-10-31 11:27:42 -0300

    Create handler.go and move handler functions

--- api/api.go
+++ api/api.go
@@ -1,9 +1,6 @@
 package api
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/adrian-feijo-fagundes/my-api-golang/db"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
@@ -35,43 +32,4 @@ func (api *API) ConfigureRoutes() {
 
 func (api *API) Start() error {
 	return api.Echo.Start(":8080")
-}
-
-// Handler
-func (api *API) getStudents(c echo.Context) error {
-	students, err := api.DB.GetStudents()
-	if err != nil {
-		return c.String(http.StatusNotFound, "Failed to get students")
-	}
-	return c.JSON(http.StatusOK, students)
-}
-func (api *API) createStudent(c echo.Context) error {
-	student := db.Student{}
-	if err := c.Bind(&student); err != nil {
-		return err
-	}
-	if err := api.DB.AddStudent(student); err != nil { // FORMA "simplificada de tratar um erro"
-		return c.String(http.StatusInternalServerError, "Error to create students\n")
-	}
-
-	return c.String(http.StatusOK, "Create students\n")
-}
-func (api *API) getStudent(c echo.Context) error {
-	id := c.Param("id") // Pega o parametro que foi passado
-	message := "GET " + getId(id)
-	return c.String(http.StatusOK, message)
-}
-func (api *API) updateStudent(c echo.Context) error {
-	id := c.Param("id")
-	message := "UPDATE " + getId(id)
-	return c.String(http.StatusOK, message)
-}
-func (api *API) deleteStudent(c echo.Context) error {
-	id := c.Param("id")
-	message := "DELETE " + getId(id)
-	return c.String(http.StatusOK, message)
-}
-
-func getId(id string) string {
-	return fmt.Sprintf("%s student \n", id)
 }
